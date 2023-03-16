aef75051b38de6d2c0bffc88f07db0fa39b227b3
Kholoudxs55kh <kfattem@gmail.com>
2023-03-15 22:46:07 -0700

    Funnc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- * Using - Printf
- *
- * return : 0   
+ * main - Entry point
+ * Return: 0
  */
-
 int main(void)
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return 0; 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
