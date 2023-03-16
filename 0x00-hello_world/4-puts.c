e2075cabc1ff28929ca20d5dd662d6eb2ed58ad6
Kholoudxs55kh <kfattem@gmail.com>
2023-03-15 21:38:51 -0700

    Put_Func.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){ 
+
+    char Mystr[] = " "Programming is like building a multilingual puzzle" ";
+    
+    puts(Mystr); 
+/*
+ * puts("\"Programming is like building a multilingual puzzle");
+*/
+return 0;
+
+}
