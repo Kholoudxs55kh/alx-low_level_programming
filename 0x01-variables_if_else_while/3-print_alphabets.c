f1aa1248ad8c9ff155c44f9d6fb7c0822af2d2bc
Kholoudxs55kh <kfattem@gmail.com>
2023-03-17 02:59:21 -0700

    ThirdEdit101

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-	char Cukur = 'a';
-	while (Cukur <= 'z')
-	{
-	putchar(Cukur);
-	Cukur++;
-	};
-	Cukur = 'A';
-	while (Cukur <= 'Z')
-	{
-	putchar(Cukur);
-	Cukur++;
-	}	
-	putchar('\n');
-	return (0);
+char Cukur = 'a';
+while (Cukur <= 'z')
+{
+putchar(Cukur);
+Cukur++;
+};
+Cukur = 'A';
+while (Cukur <= 'Z')
+{
+putchar(Cukur);
+Cukur++;
+}	
+putchar('\n');
+return (0);
 }
