906edc5fba1543505d513b525b227abb484a98a1
Kholoudxs55kh <kfattem@gmail.com>
2023-03-17 00:56:05 -0700

    Finally

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int k = 0;
+while (k < 10)
+{
+putchar(k);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
