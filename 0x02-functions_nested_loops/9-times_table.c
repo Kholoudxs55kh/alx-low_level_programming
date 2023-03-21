106557d946c3250244f86069bbaff76799ef786a
Kholoudxs55kh <kfattem@gmail.com>
2023-03-21 12:29:57 -0700

    Ninth

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *times_table - Entry Point
+ *
+ *Return: 0
+ */
+void times_table(void)
+{
+	int a, b, sum;
+	while (a <= 9)
+	{
+		_putchar(48);
+		while (b <= 9)
+		{
+			sum = a * b;
+			_putchar(44);
+			_putchar(32);
+			if ( sum <= 9)
+			{
+			_putchar(32);
+			_putchar(sum + '0');
+			}
+			a++;
+		}
+		b++
+	}
+}
