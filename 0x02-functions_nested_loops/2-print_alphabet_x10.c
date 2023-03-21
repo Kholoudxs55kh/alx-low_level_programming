cd0f68e6bd54795c1e7238bbc04970b57501e9d3
Kholoudxs55kh <kfattem@gmail.com>
2023-03-21 04:25:32 -0700

    IncreamentTimes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry Point
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char a = 'a';
+int Times = 10;
+while ( Times <= 10) {
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+_putchar('\n');
+}
+Times++
+}
+}
