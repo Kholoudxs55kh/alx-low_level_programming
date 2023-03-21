e9aed0c4eec2801400c2682a26f7534164014070
Kholoudxs55kh <kfattem@gmail.com>
2023-03-21 12:01:20 -0700

    Ninth

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,19 +6,19 @@
  */
 void jack_bauer(void)
 {
-	int H;
-	int S;
+int H;
+int S;
 
-	for (H = 0; H < 24; H++)
-	{
-		for (S = 0; S < 60 ; S++)
-		{
-			_putchar (S / 10 + '0');
-			_putchar (S % 10 + '0');
-			_putchar (58);
-		_putchar (H / 10 + '0');
-		_putchar (H % 10 + '0');
-		_putchar ('\n');
+for (H = 0; H <= 23; H++)
+{
+for (S = 0; S <= 59; S++)
+{
+_putchar (H / 10 + '0');
+_putchar (H % 10 + '0');
+_putchar (':');
+_putchar (S / 10 + '0');
+_putchar (S % 10 + '0');
+_putchar ('\n');
 }
 }
 }
