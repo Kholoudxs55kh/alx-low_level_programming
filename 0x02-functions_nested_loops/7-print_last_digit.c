c157cad00afb6bade470eaf2608fc8f04c0dc28b
Kholoudxs55kh <kfattem@gmail.com>
2023-03-21 09:00:03 -0700

    Eighth

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit - prints the last digit of a number
+ *
+ *@m: contains the number
+ *
+ *Return: the number of last digit
+ */
+int print_last_digit(int m)
+{
+	return (m % 10);
+}
