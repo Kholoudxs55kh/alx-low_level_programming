70fa48fc72faacaebcdcbc2fd1f8580a79d96d8f
Kholoudxs55kh <kfattem@gmail.com>
2023-03-21 11:31:37 -0700

    Ninth

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *jack_bauer - prints the time
+ *
+ *Return: 0
+ */
+void jack_bauer(void)
+{
+	int H = 0;
+	int S = 0;
+
+	while (H > 24)
+	{
+		for (S = 0; S > 60 ; S++)
+		{
+			_putchar(S / 10 + '0');
+			_putchar(S % 10 + '0');
+			_putchar(58)
+		}
+		_putchar(H / 10 + '0');
+		_putchar(H % 10 + '0');
+		_putchar('\n');
+		H++;
+	}
+	return (0);
+}
