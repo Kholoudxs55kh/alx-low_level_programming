032741c890d26522a75c6ae5d8200db9235d940a
Kholoudxs55kh <kfattem@gmail.com>
2023-03-22 14:00:22 -0700

    ADVANCED

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	long int Repeat, First = 1, Second = 2, Sum = 0;
-for (Repeat = 0 ; Repeat < 50 ; Repeat++)
+for (Repeat = 0 ; Repeat < 49 ; Repeat++)
 {
 printf("%ld, ", First);
 Sum = First + Second;
 First = Second;
 Second = Sum;
-if (Repeat == 49)
+if (Repeat == 48)
 printf("%ld\n", Second);
 }
 return (0);
