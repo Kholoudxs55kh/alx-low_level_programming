202e4b4a3f141cf327fef11228016205d8b495f8
Kholoudxs55kh <kfattem@gmail.com>
2023-03-23 15:08:47 -0700

    blabla

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		else if (n % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(n + '0');
+			_putchar(' ');
+		}
+	}
+}
