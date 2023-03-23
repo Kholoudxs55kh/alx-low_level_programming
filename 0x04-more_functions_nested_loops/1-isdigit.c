2d64ac3fc6f208fcbf20ec82245ab282fb12c7c9
Kholoudxs55kh <kfattem@gmail.com>
2023-03-23 05:01:20 -0700

    SecondTask

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _isdigit - prints if the digit is 0
+ *_isdigit - prints if the digit is 0
  *
  *@c: checks the Variable
  *
- * Return: 1 if c is a digit
+ *Return: 1 if c is a digit
  */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9');
-		return (1)
+		return (1);
 	else 
 		return (0);
 }
