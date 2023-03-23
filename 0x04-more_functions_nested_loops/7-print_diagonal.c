523ee035bb6303c5e0601481fbefba9a812bae4e
Kholoudxs55kh <kfattem@gmail.com>
2023-03-23 13:15:21 -0700

    bla bla

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,11 +8,20 @@
  */
 void print_diagonal(int n)
 {
-	while (n > 0)
+		int num_lines;
+
+	if (n <= 0)
 	{
-		_putchar(' ');
-		n++;
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\\');
-	_putchar('\n');
+	else
+	{
+				for (num_lines = 1; num_lines <= row - 1; num_lines++)
+				{
+				_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
 }
