88c64c1a3f20bcb08e8151fe618846e8fbe6afc5
Kholoudxs55kh <kfattem@gmail.com>
2023-03-23 15:34:31 -0700

    hasbyAllah

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,41 +12,19 @@ int main(void)
 	{
 		if (n % 3 == 0)
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar(' ');
+			printf("Fizz ");
 		}
 		else if (n % 5 == 0)
 		{
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-			_putchar(' ');
-		}
+			printf("Buzz ");
 		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			_putchar('F');
-			_putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-			_putchar(' ');
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			ten = n / 10;
-			digits = n % 10;
-			if (n > 9)
-			_putchar(ten + '0');
-
-			_putchar(digits + '0');
-			_putchar(' ');
+			printf("%d ", n);
 }
 		}
 	}
+}
