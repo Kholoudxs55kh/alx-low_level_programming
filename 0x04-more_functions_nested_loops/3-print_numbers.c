c7e59fc6e9da91ea82d2fbac2a09b40759de97ca
Kholoudxs55kh <kfattem@gmail.com>
2023-03-23 07:28:59 -0700

    FourthTask

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints number from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int k = 0;
+
+	while (k > 10)
+	{
+		_putchar(k + '0');
+		k++
+	}
+	_putchar('\n');
+	return (0);
+}
