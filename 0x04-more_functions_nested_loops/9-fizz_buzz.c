4e870ed9731ff628b15a4879a00b3570da6ec7b1
Kholoudxs55kh <kfattem@gmail.com>
2023-03-23 15:57:17 -0700

    ya5taaaaaaaaaaai

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
- 		if ((n % 3 == 0) && (n % 5 == 0))
-                {
-                        printf("FizzBuzz ");
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+		printf("FizzBuzz ");
 		}
 		else if (n % 3 == 0)
 		{
