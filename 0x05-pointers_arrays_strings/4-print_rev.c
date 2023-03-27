0482ea93aeeca1274143a224ea1359eaf4597b4f
Kholoudxs55kh <kfattem@gmail.com>
2023-03-27 13:49:12 -0700

    FifthTask

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ *
+ * @s: contains the string
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int k;
+
+	while (k == '\0')
+	{
+		_putchar(s[k]);
+		--k;
+	}
+	_putchar('\n');
+}
