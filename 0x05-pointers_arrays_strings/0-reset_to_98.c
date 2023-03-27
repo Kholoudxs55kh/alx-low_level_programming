a6a7967c127a6ab16daef010c8c0d03d8f34100b
Kholoudxs55kh <kfattem@gmail.com>
2023-03-27 04:43:24 -0700

    FirstTry

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of the variable to 98
+ *
+ * @n: contains the value
+ *
+ * Return: 0
+ *
+ */
+void reset_to_98(int *n)
+{
+	int *ptr;
+
+	ptr = &n;
+	*ptr = 98;
+}
