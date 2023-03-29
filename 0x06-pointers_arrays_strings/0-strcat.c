2f46d96cfcf73d4cc9c1ca6518eaf00909bebd23
Kholoudxs55kh <kfattem@gmail.com>
2023-03-29 13:35:26 -0700

    aybla7

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: contains a string
+ * @src: contains a string
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr[];
+	
+	ptr = dest + src;
+	return (*ptr);
+}
