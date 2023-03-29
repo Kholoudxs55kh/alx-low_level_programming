c58d5cd5ce1c44dee843224c42b6c9fbea12c15c
Kholoudxs55kh <kfattem@gmail.com>
2023-03-29 16:37:43 -0700

    Try10101

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int  m;
+	int m;
 
 	for (m = 0; m < n && src[m] != '\0'; m++)
 	{
 		dest[m] = src[m];
 	}
+	while (m < n)
+	{
 	dest[m] = '\0';
+	m++;
+	}
 	return (dest);
 }
