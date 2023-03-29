d8c25807acd48230951cd118dec8b40c4126ec60
Kholoudxs55kh <kfattem@gmail.com>
2023-03-29 16:33:48 -0700

    ThirdTask101

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int k = strlen(dest), m;
+	int  m;
 
 	for (m = 0; m < n && src[m] != '\0'; m++)
 	{
-		dest[k] = src[m];
+		dest[m] = src[m];
 	}
 	dest[m] = '\0';
 	return (dest);
