14599bae2efd7870b262b14c20debdb26e6f86a1
Kholoudxs55kh <kfattem@gmail.com>
2023-03-29 15:45:47 -0700

    TaskTry

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: contains both strings isa
+ * @src: contains one of them
+ * @n: contains the bytes of src
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int k = strlen(dest), m;
+
+	while (m < n)
+	{
+	for (m = 0; src[m] != '\0'; m++)
+	{
+	dest[k + m] = src[m];
+	}
+	dest[k + m] = '\0';
+	return (dest);
