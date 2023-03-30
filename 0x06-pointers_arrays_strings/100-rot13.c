8d0e0c7f5a0115bd93087aa229f5b29f85618699
Kholoudxs55kh <kfattem@gmail.com>
2023-03-29 21:57:08 -0700

    Advaa

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string into rot13
+ *
+ * @k: contains the string
+ *
+ * Return: roted string
+ */
+char *rot13(char *k)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int h = 0, m = 0;
+
+	while (k[h] != '\0')
+	{
+		for (m = 0; m < 5; m++)
+		{
+			if (k[h] == a[m])
+			{
+				k[h] = rot13[m];
+				break;
+			}
+		}
+		h++;
+	}
+	return (k);
+}
