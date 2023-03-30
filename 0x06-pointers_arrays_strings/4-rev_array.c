de369d94665daf5f919c55d8457cec9b80d36b0b
Kholoudxs55kh <kfattem@gmail.com>
2023-03-29 19:53:14 -0700

    FifthTask

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ *
+ * @a: the array
+ * @n: the number of elements
+ *
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+	int k = 0, h;
+
+	while (k <= n - 1)
+	{
+		h = a[k];
+		a[k++] = a[n];
+		a[n--] = h;
+	}
+}
+
