4b55da411ccb8854bc681e602bc1e4c4f3caa4dc
Kholoudxs55kh <kfattem@gmail.com>
2023-03-29 20:09:58 -0700

    SexthTask

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *string_toupper -  changes all lowercase letters of a string to uppercase.
+ *
+ * Return: the inputs in upper cases
+ */
+char *string_toupper(char *k)
+{
+	int i;
+
+	for (i = 0; k[i]!='\0'; i++)
+	{
+	if(k[i] >= 'a' && k[i] <= 'z')
+		k[i] = k[i] - 32;
+	}
+	return (k);
+}
