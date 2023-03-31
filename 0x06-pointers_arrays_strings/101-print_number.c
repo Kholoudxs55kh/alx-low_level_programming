22f5863eee2cb9ca47cd8ae0179074d1412c7b11
Kholoudxs55kh <kfattem@gmail.com>
2023-03-30 19:18:17 -0700

    continue

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_number - prints an integer using putchar
+ *
+ * @n: contains the integer
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar ('-');
+		num = -n;
+	}
+	else if (n < 10)
+	{
+		_putchar(n % 10 + '0');
+	}
+	else if (n > 9)
+	{
+		_putchar(
+				/*
+				 * to be continued
+				 */
+				}
+				}
