a02bab3e9e5a4848ec01b21633b652809655a474
Kholoudxs55kh <kfattem@gmail.com>
2023-04-03 16:43:38 -0700

    TryAgain

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard.
+ * 
+ * @a: contains bla bla
+ *
+ * Return: Void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int k, h;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (h = 0; h < 8; h++)
+			_putchar (a[i][j]);
+
+	_putchar('\0');
+	}
+}
