bc2fa50f4691322371002df7caa8cbdab4cab175
Kholoudxs55kh <kfattem@gmail.com>
2023-04-03 17:21:43 -0700

    yyyyyyyy

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,20 +7,23 @@
  *
  * Return: pointer to the beginning of the located substring, or NULL
  */
-char *_strstr(char *haystack, char *needle)
+char  *_strstr(char *haystack, char *needle)
 {
-while (*haystack != '\0')
-{
-while (*haystack != '\0' && *needle != '\0' && *needle == *haystack)
-{
-haystack++;
-needle++;
-}
-if (!*needle)
-{
-return (haystack);
-}
-haystack += 1;
-}
-return (0);
+	char *first, *second;
+
+	while (*haystack != '\0')
+	{
+		first = haystack;
+		second = needle;
+
+		while (*haystack != '\0' && *second != '\0' && *haystack == *second)
+		{
+			haystack++;
+			second++;
+		}
+		if (*second == '\0')
+			return (first);
+		haystack = first + 1;
+	}
+	return (0);
 }
