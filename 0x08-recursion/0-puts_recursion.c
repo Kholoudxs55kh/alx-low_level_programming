3c3195f7c616f029603cc1c1a87524f5611b3dca
Kholoudxs55kh <kfattem@gmail.com>
2023-04-05 15:08:08 -0700

    FirstTask

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string followed by a new line 
+ * _puts_recursion - prints a string followed by a new line
  * @s: contains the string
  * Return: void
  */
 void _puts_recursion(char *s)
 {
 	int k;
+
 	for (k = 0; s[k] != '\0'; k++)
 	{
 		_putchar(*(s + k));
