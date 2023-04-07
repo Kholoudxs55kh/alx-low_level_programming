a591c8228153555f16580c12f3700dff7dc629c4
Kholoudxs55kh <kfattem@gmail.com>
2023-04-06 20:32:05 -0700

    FifthTaskLastMandatory

---
+++ 0x09-static_libraries/0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - contains the programme
+ * @argc: considered as the index of the command line
+ * @argv: has the value of those indexes
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int nateg = 0, k;
+
+	for (k = 1; k < argc; k++)
+	{
+		if (!isdigit(*argv[k]))
+	{
+		printf("Error\n");
+			return (1);
+	}
+	nateg = nateg + atoi(argv[k]);
+	}
+	printf("%d\n", nateg);
+
+	return (0);
+}
