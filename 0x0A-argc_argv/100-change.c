c84cc66718b4ab1d7073f70b60fee28b5a7ddb13
Kholoudxs55kh <kfattem@gmail.com>
2023-04-07 13:17:08 -0700

    adv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - contains the programme
+ * @argc: index msln
+ * @argv: value of index msln
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+
+	return (0);
+}
