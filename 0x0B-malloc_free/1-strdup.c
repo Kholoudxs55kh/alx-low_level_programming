5a9bfdcc98f4f2bd053ab3869c8ad2215c9ebcf0
Kholoudxs55kh <kfattem@gmail.com>
2023-04-11 16:00:29 -0700

    SecondTask

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _sttdup - returns a pointer to a newly allocated space in memory,
+ * _strdup - returns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter.
  * @str: the string
  * Return: pointer cstr
