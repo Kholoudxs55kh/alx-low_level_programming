6b90b685faf18f9ff20e94d91550200541f77a75
Kholoudxs55kh <kfattem@gmail.com>
2023-04-15 20:15:06 -0700

    LastMandatory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: two dim array
+ * @height: the height
+ * Return: VOID
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		if (height >= 0)
+		{
+			free(grid[height]);
+		}
+		free(grid);
+	}
+}
