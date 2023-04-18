096d7cbe99d414e8f4d34a6f5b3bc37e94c8861f
Kholoudxs55kh <kfattem@gmail.com>
2023-04-17 17:10:20 -0700

    FifthTask

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * dog_t *new_dog - creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: NULL, or pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	dog_t *Newdog;
+
+	Newdog = malloc(sizeof(dog_t));
+
+	if (Newdog == NULL)
+		return (NULL);
+
+	Newdog->name = malloc((strlen(name) + 1) * sizeof(char));
+	Newdog->owner = malloc((strlen(owner) + 1) * sizeof(char));
+
+	Newdog->age = age;
+
+	strcpy(Newdog->name, name);
+	strcpy(Newdog->owner, owner);
+
+	return (Newdog);
+}
