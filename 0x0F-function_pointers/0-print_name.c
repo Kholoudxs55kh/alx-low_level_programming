d7c0993231ae4f9190257e653c5549dc9f847bd5
Kholoudxs55kh <kfattem@gmail.com>
2023-04-19 12:33:21 -0700

    FirstTaskFix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,12 @@
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name
- * @name: input name
- * @f: function pointer
- *
- * Return: no return
+ * @name: the name
+ * @f: the pointer
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (name != NULL && f != NULL)
+		(*f)(name);
 }
