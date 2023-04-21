b6865373cc8babbc6909486680ff0af64a2b5b80
Kholoudxs55kh <kfattem@gmail.com>
2023-04-20 17:07:49 -0700

    SecTask

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers
+ * @n: the inputs
+ * @separator: string to be printed
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+
+	va_list args;
+	va_start(args, n);
+
+	if (separator == NULL)
+		return;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator && x < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(args);
+}
