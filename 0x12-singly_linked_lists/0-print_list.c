c0e1e079f817bcb7cf1042e1245c587940920fde
Kholoudxs55kh <kfattem@gmail.com>
2023-04-29 12:44:54 -0700

    FirstLateTask

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer
- * Retirn: the number of nodes
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
