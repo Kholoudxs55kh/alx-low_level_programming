3e682e2dac16c55dfe4b46069a9f26beb06c276d
Kholoudxs55kh <kfattem@gmail.com>
2023-04-30 15:35:51 -0700

    FirstAdv

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+__attribute__ ((__constructor__)) void print_(void);
 /**
- *print_ - prints a msg before the main
+ * print_ - prints a msg before the main
  */
 
-__attribute__ ((__constructor__)) void print_(void)
+void print_(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
