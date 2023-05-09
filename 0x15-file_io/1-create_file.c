60fea35047ed95c0d35b84239fd2805f539507d7
Kholoudxs55kh <kfattem@gmail.com>
2023-05-08 19:46:14 -0700

    Task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_WRONLY| O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (!file)
 		return (-1);
