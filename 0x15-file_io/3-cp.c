8792e4ae49de2c1340f4d33721f67c701688ef37
Kholoudxs55kh <kfattem@gmail.com>
2023-05-09 03:19:18 -0700

    Trying

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,42 +7,42 @@
  */
 int main(int argc, char *argv[])
 {
-    int fd1, fd2, buf, buff;
-    char *file_from = argv[1], *file_to = argv[2], buffer[1024];
+	int fd1, fd2, buf, buff;
+	char *file_from = argv[1], *file_to = argv[2], buffer[1024];
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-        exit(97);
-    }
-    fd1 = open(file_from, O_RDONLY);
-    fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-    buf = read(fd1, buffer, 1024);
-    buff = write(fd2, buffer, buf);
-    while (buf)
-    {
-    if ((!fd1) || (!buf))
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s \n", file_from);
-        exit(98);
-    }
-    if ((!fd2) || (!buff))
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s \n", file_to);
-        exit(99);
-    }
-    }
-    if (!(close(fd1)))
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-        exit(100);
-    }
-    if (!(close(fd2)))
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-        exit(100);
-    }
-    close(fd1);
-    close(fd2);
-    return (0);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	fd1 = open(file_from, O_RDONLY);
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	buf = read(fd1, buffer, 1024);
+	buff = write(fd2, buffer, buf);
+	while (buf)
+	{
+		if ((!fd1) || (!buf))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s \n", file_from);
+			exit(98);
+		}
+		if ((!fd2) || (!buff))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s \n", file_to);
+			exit(99);
+		}
+	}
+	if (!(close(fd1)))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (!(close(fd2)))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	close(fd1);
+	close(fd2);
+	return (0);
 }
