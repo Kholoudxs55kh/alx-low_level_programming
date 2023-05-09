8017ee8e053bf0569cb421f6adade947d2bb2ac0
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-05-09 13:09:09 +0300

    Trying

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-    int fd1, fd2, cls = close(fd1), clss = close(fd2), buf = 1024, buff;
+    int fd1, fd2, buf = 1024, buff;
     char *file_from = argv[1], *file_to = argv[2], buffer[1024];
 
     if (argc != 3)
@@ -15,39 +15,26 @@ int main(int argc, char *argv[])
         dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
         exit(97);
     }
-    fd1 = open(file_from, O_WRONLY);
+    fd1 = open(file_from, O_RDONLY);
     fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-    if (!fd1)
+    buf = read(fd1, buffer, 1024);
+    buff = write(fd2, buffer, buf);
+    if ((!fd1) || (!buf))
     {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s \n", file_from);
         exit(98);
     }
-    if (!fd2)
+    if ((!fd2) || (!buff))
     {
         dprintf(STDERR_FILENO, "Error: Can't write to %s \n", file_to);
         exit(99);
     }
-    while (buf)
-    {
-        buf = read(fd1, buffer, 1024);
-        if (!buf)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't read from file %s \n", file_from);
-            exit(98);
-        }
-        buff = write(fd2, buffer, buf);
-        if (!buff)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s \n", file_to);
-            exit(99);
-        }
-    }
-    if (!cls)
+    if (!(close(fd1)))
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
         exit(100);
     }
-    if (!clss)
+    if (!(close(fd2)))
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
         exit(100);
