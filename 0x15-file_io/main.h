f59d54fc385fddf7ea7ba2f364498c3468315a1e
root <root@Kholoud>
2023-05-09 03:19:07 +0300

    FirstTask

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,16 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+#define O_RDONLY 00
+#define O_WRONLY 01
+#define O_RDWR 02
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
