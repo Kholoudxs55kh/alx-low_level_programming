fa48472b172944b5000535d8aed2d4e97769fac4
root <root@Kholoud>
2023-05-09 04:59:44 +0300

    SecTask

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, x, k;
 
-	k = strlen(text_content);
+    if (text_content)
+	    k = strlen(text_content);
+		   
 
 	if (!filename)
 		return (-1);
