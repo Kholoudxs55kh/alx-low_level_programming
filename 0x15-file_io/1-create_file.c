e528637687857005538c40bd55097c95c21e0be6
Kholoudxs55kh <kfattem@gmail.com>
2023-05-09 05:43:52 +0300

    Task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (!file)
 		return (-1);
-        
-    if (!text_content)
-        text_content = "";
 
 if (text_content)
 {
@@ -26,8 +23,6 @@ if (text_content)
 
 	if (!x)
 		return (-1);
-    
-
 }
 	close(file);
 
