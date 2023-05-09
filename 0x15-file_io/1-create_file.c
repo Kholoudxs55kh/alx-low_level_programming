7523b53b1db02c3d6b49b3e35ebc76adc3c31d58
root <root@Kholoud>
2023-05-09 04:44:44 +0300

    SecTask

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT, 0600);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (!file)
 		return (-1);
