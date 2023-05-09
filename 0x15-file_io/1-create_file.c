5f4a93da6a89291ccaad1ca6b371444c490049d6
Kholoudxs55kh <kfattem@gmail.com>
2023-05-09 05:19:00 +0300

    SecTask

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,8 @@ int create_file(const char *filename, char *text_content)
 	if (!file)
 		return (-1);
 
+    if (!text_content)
+		text_content = "";
 
 	x = write(file, text_content, strlen(text_content));
 
