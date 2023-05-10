56ee77b5125d64da8467914ed3456b275cd38dc6
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-05-10 03:09:29 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,7 +16,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int egzit(int argc, char *argv[], int e97, int e89, int e99, int ef100, int es100);
 int main(int argc, char *argv[]);
 
 #endif
