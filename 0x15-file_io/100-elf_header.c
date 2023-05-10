fe81947b2b6faddfcf117239ea72b0519668d962
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-05-10 09:05:40 +0300

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,27 +8,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int x;
-	char *ptr;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-	x = atoi(argv[1]);
-
-	if (x < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	while (x--)
-	{
-		printf("%02x%c", *ptr++ & 0xff, x ? ' ' : '\n');
-	}
-
-	return (0);
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+		return (1);
 }
