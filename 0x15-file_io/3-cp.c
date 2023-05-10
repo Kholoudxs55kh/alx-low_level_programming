59c6f92b1db4ecac421b159033dd73cea9f25067
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-05-10 03:16:40 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,45 @@
 #include "main.h"
 /**
+ * egzit_ - exits
+ * @e97: .
+ * @e98: .
+ * @e99: .
+ * @ef100: .
+ * @es100: .
+ * Return: .
+*/
+int egzit(int argc, int fd1, int fd2, int cls1, int cls2)
+{
+	cls1 = (close(fd1) == -1);
+	cls2 = (close(fd2) == -1);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (fd1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	if (fd2)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	if (cls1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	if (cls2)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+
+}
+/**
  * main - the program
  * @argc: the arguements
  * @argv: the inputs
@@ -10,72 +50,39 @@ int main(int argc, char *argv[])
 	int fd1, fd2, buf;
 	char *file_from, *file_to, buffer[1024];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	/*remove*/
+	egzit(1, 0, 0, 0, 0);
 	file_from = argv[1];
 	file_to = argv[2];
-
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
+		egzit(0, 1, 0, 0, 0);
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
+	egzit(0, 0, 1, 0, 0);
 	while ((buf = read(fd1, buffer, 1024)))
 	{
-
 		if ((buf == -1))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			if (close(fd2) == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-				exit(100);
-			}
+				egzit(0, 0, 0, 0, 1);
 			if (close(fd1) == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-				exit(100);
-			}
+				egzit(0, 0, 0, 1, 0);
 			exit(98);
 		}
 		if (buf != write(fd2, buffer, buf))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			if (close(fd2) == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-				exit(100);
-			}
+				egzit(0, 0, 0, 0, 1);
 			if (close(fd1) == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-				exit(100);
-			}
+				egzit(0, 0, 0, 1, 0);
 			exit(99);
 		}
 	}
 	if (close(fd2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
+		egzit(0, 0, 0, 0, 1);
 	if (close(fd1) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
+		egzit(0, 0, 0, 1, 0);
 	return (0);
 }
