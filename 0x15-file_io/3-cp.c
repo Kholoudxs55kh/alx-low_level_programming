6ca33d61e42926452c3d5b90f55459f625006bdd
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-05-10 03:24:00 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  * @es100: .
  * Return: .
 */
-int egzit(int argc, int fd1, int fd2, int cls1, int cls2)
+int egzit(int argc, int fd1, int fd2, int cls1, int cls2, char *argv)
 {
 	cls1 = (close(fd1) == -1);
 	cls2 = (close(fd2) == -1);
@@ -19,12 +19,12 @@ int egzit(int argc, int fd1, int fd2, int cls1, int cls2)
 	}
 	if (fd1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
 		exit(98);
 	}
 	if (fd2)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
 		exit(99);
 	}
 	if (cls1)
@@ -50,39 +50,39 @@ int main(int argc, char *argv[])
 	int fd1, fd2, buf;
 	char *file_from, *file_to, buffer[1024];
 
-	egzit(1, 0, 0, 0, 0);
+	egzit(1, 0, 0, 0, 0, argv);
 	file_from = argv[1];
 	file_to = argv[2];
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-		egzit(0, 1, 0, 0, 0);
+		egzit(0, 1, 0, 0, 0, argv);
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (fd2 == -1)
-	egzit(0, 0, 1, 0, 0);
+	egzit(0, 0, 1, 0, 0, argv);
 	while ((buf = read(fd1, buffer, 1024)))
 	{
 		if ((buf == -1))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			if (close(fd2) == -1)
-				egzit(0, 0, 0, 0, 1);
+				egzit(0, 0, 0, 0, 1, argv);
 			if (close(fd1) == -1)
-				egzit(0, 0, 0, 1, 0);
+				egzit(0, 0, 0, 1, 0, argv);
 			exit(98);
 		}
 		if (buf != write(fd2, buffer, buf))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			if (close(fd2) == -1)
-				egzit(0, 0, 0, 0, 1);
+				egzit(0, 0, 0, 0, 1, argv);
 			if (close(fd1) == -1)
-				egzit(0, 0, 0, 1, 0);
+				egzit(0, 0, 0, 1, 0, argv);
 			exit(99);
 		}
 	}
 	if (close(fd2) == -1)
-		egzit(0, 0, 0, 0, 1);
+		egzit(0, 0, 0, 0, 1, argv);
 	if (close(fd1) == -1)
-		egzit(0, 0, 0, 1, 0);
+		egzit(0, 0, 0, 1, 0, argv);
 	return (0);
 }
