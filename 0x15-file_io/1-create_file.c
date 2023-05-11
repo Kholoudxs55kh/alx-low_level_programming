58e70e7c83adddf62d2f2010d48b1734fd37af66
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-05-11 08:07:06 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		x = write(file, text_content, strlen(text_content));
 
-		if (!x)
+		if ((x == -1) || (x == 0))
 			return (-1);
 	}
 	close(file);
