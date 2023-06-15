f20b9ee6cb1843d26c7bfb7cd5e10c6a534577e0
Kholoudxs55kh <kfattem@gmail.com>
2023-06-15 15:31:10 -0700

    FifthTask

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - .
+ * @head: .
+ * Return: .
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *nodeToFree = head;
+
+	while(nodeToFree)
+	{
+		head = nodeToFree->next;
+		free(nodeToFree);
+		nodeToFree = head;
+	}
+}
