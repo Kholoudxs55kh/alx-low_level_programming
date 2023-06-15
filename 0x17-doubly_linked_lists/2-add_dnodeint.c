74cf23040a68040c6130c2e1ffdb8ad12ab9e280
Kholoudxs55kh <kfattem@gmail.com>
2023-06-15 15:07:32 -0700

    ThirdTask

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
  * add_dnodeint - .
- * @h: .
+ * @head: .
  * @n: .
  * Return: .
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = (dlistint_t*)malloc(sizeof(dlistint_t));
+	dlistint_t *new_node = (dlistint_t)malloc(sizeof(dlistint_t));
 
 	if (!new_node)
 		return (NULL);
