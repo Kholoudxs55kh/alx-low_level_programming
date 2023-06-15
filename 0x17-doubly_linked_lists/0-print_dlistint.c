3df207d4adbd2aac07e1b00dd534609f357cb917
Kholoudxs55kh <kfattem@gmail.com>
2023-06-15 14:50:31 -0700

    FirstTaskFixing

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,10 +6,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	int node_num = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		node_num++;
 	}
-	return (0);
+	return (node_num);
 }
