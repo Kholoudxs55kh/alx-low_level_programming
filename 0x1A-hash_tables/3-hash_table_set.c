27144b0627e018a1b2646a4959c938307a997b0e
Kholoudxs55kh <kfattem@gmail.com>
2023-07-09 00:41:56 -0700

    editing

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+hash_table_t *create_new_node(const char *key, const char *value)
+{
+
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table to add in
+ * @key: the key to the val
+ * @value: the value associated to the key
+ * Return: 1, 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_table_t *hash_table
+
+
+
+	return (1);
+}
